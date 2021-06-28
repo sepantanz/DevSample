d9437620d66f9c31799b5b23976a77ee083073c4
sepantanz <sepanta.nz@gmail.com>
2021-06-28 13:10:01 +0430

    index and contact pages added

--- DevSample_MVC/Controllers/HomeController.cs
+++ DevSample_MVC/Controllers/HomeController.cs
@@ -1,21 +1,13 @@
 ﻿using DevSample_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevSample_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,7 +15,7 @@ namespace DevSample_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
